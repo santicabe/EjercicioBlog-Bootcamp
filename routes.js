0ede8db4084c3f9f5b184bb57e3701990ec9ffbe
santicabe <santiago.cabe@gmail.com>
2021-04-08 14:54:25 -0300

    administrador vista

--- routes.js
+++ routes.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/", (req, res) => { res.send("Pagina de inicio"); });
-
+router.get("/", (req, res) => {
+  res.send("Pagina de inicio");
+});
 
 router.get("/home", (req, res) => {
- 
   res.render("home");
 });
 
 router.get("/articulo/:id", (req, res) => {});
 
 router.get("/administrador", (req, res) => {
-  res.send("administrador");
+  res.render("admin");
 });
 
 router.post("/administrador", (req, res) => {});
