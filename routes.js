ae4b810707dc4c1a68e08f0aacc7ad38350718e3
santicabe <santiago.cabe@gmail.com>
2021-04-08 15:58:11 -0300

    Avance de hormiga

--- routes.js
+++ routes.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql2")
+
+const connection = mysql.createConnection({
+  host: "Local Host"
+  user: "root"
+  password: "root"
+  database:"ejercicio21"
+})
 
 router.get("/", (req, res) => {
   res.send("Pagina de inicio");
 });
 
 router.get("/home", (req, res) => {
+  connection.query("SELECT * FROM articulos", (err, result) => {
+
+    if(err) {
+      throw err
+    } else {
+
+    }
+
+  })
   res.render("home");
 });
+
+
+
+
+
 
 router.get("/articulo/:id", (req, res) => {});
 
